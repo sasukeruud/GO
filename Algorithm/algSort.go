c61df06614c9b16a1d3ffb59245f13a38f7b2429
Robin <robin1998@hotmail.no>
2022-01-12 17:59:25 +0100

    small fixes to syntax

--- Algorithm/algSort.go
+++ Algorithm/algSort.go
@@ -1,36 +1,34 @@
 package algSort
 
-import(
-	"fmt"
-)
+//"fmt"
 
 //Function to swap two int
-func swapInt(a,b int) int{
+func swapInt(a, b int) {
 	var t int = a
 	a = b
 	b = t
 }
 
 /*
-* Partition function for quickSort 
-*/
-func partition(low, high, arr[]int int) int{
-	var(
+* Partition function for quickSort
+ */
+func partition(arr []int, low, high int) int {
+	var (
 		piviot int = arr[high]
-		i int = low - 1
+		i      int = low - 1
 	)
 
-	for j := low; j <= high - 1; j++{
-		if arr[j] < piviot{
+	for j := low; j <= high-1; j++ {
+		if arr[j] < piviot {
 			i++
-			swap(arr[i], arr[j])
+			swapInt(arr[i], arr[j])
 		}
 	}
 
-	swap(arr[i+1], arr[high])
+	swapInt(arr[i+1], arr[high])
 	return i + 1
 }
 
-func quickSort() void{
+func quickSort() {
 
 }
